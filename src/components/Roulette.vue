dc6d93e2fb34b6512016fe4edac15aafc9595211
dani7cl <daniel19.cl@gmail.com>
2019-09-08 21:48:20 +0200

    Rotating Pie

--- src/components/Roulette.vue
+++ src/components/Roulette.vue
@@ -1,5 +1,17 @@
 <template>
-  
+  <div class="roulete">
+    <div class="pie animated">
+      <div class="animated">
+        <div class="segment"></div>
+        <div class="segment"></div>
+        <div class="segment"></div>
+      </div>
+      <p class="name">Dani</p>
+      <p class="name">Jose</p>
+      <p class="name">Lorena</p>
+      <p class="name">Alba</p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,10 +19,70 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Roulette extends Vue {
-  @Prop() private msg!: string;
 }
 </script>
 
 <style scoped lang="scss">
+  .pie {
+    height: 500px;
+    width: 500px;
+    border-radius: 50%;
+    background: #d1cfc0;
+    position: relative;
+    overflow: hidden;
+    .animated {
+      width: 100%;
+      height: 100%;
+      animation: rotate 1s infinite linear;
+    }
+  }
+  .segment {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    &:first-child {
+      background: #f4f186;
+      transform: translate(50%, -50%);
+    }
+
+    &:nth-child(2) {
+      background: #90ee90;
+      transform: translate(-50%, 50%);
+    }
+
+    &:nth-child(3) {
+      background: #add8e6;
+      transform: translate(50%, 50%);
+    }
+
+    
+    @keyframes rotate {
+      from {
+        transform: rotate(0deg);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    }
+  }
+  .name {
+    position: absolute;
+    top: 30%;
+    left: 50%;
+    width: 50%;
+    text-align: center;
+    margin: 0;
+    font-weight: bold;
+    &:nth-of-type(2) {
+      left: 0%;
+    }
+    &:nth-of-type(3) {
+      top: 70%;
+    }
+    &:nth-of-type(4) {
+      top: 70%;
+      left: 0%;
+    }
+  } 
 
 </style>
